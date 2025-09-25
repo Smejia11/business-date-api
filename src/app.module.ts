34937c01ca510eeeacbc56a157ff506a5ae074ad
samejia <santiago.mejia.oquendo1@gmail.com>
2025-09-25 17:25:38 -0500

    feat: added rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { BusinessDateModule } from './business-date/infrastructura/business-date.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [BusinessDateModule],
+  imports: [
+    BusinessDateModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 50,
+        },
+      ],
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
