c10efaab417e3192b7fb67891d92150a02dd607e
samejia <santiago.mejia.oquendo1@gmail.com>
2025-09-26 09:50:50 -0500

    fix: limit params days hours

--- src/business-date/infrastructure/dto-business-date.ts
+++ src/business-date/infrastructure/dto-business-date.ts
@@ -1,4 +1,4 @@
-import { IsInt, IsOptional, IsPositive, IsISO8601 } from 'class-validator';
+import { IsInt, IsOptional, IsPositive, IsISO8601, Max } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class GetBusinessDateDto {
@@ -6,12 +6,14 @@ export class GetBusinessDateDto {
   @Type(() => Number)
   @IsInt()
   @IsPositive()
+  @Max(30, { message: 'days cannot be greater than 30' })
   days?: number;
 
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @IsPositive()
+  @Max(24, { message: 'hours cannot be greater than 24' })
   hours?: number;
 
   @IsOptional()
